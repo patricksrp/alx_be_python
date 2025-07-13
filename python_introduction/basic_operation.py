2750a92ba64d86f9de9b5fa405ae2b1de39ef994
shimirwa <shimirwaremypatrick@gmail.com>
2025-07-13 11:44:11 +0200

    upload

---
+++ python_introduction/basic_operation.py
@@ -0,0 +1,13 @@
+#aasign specific values 
+number1 =10
+number2 =5
+
+sum_result = number1 + number2
+
+difference_result = number1 - number2
+
+product_result = number1 * number2
+
+print( f"Addition of {number1} and {number2} is {sum_result}")
+print( f"substration of {number1} and {number2} is {difference_result}")
+print( f"multiplication  of {number1} and {number2} is {product_result}")
